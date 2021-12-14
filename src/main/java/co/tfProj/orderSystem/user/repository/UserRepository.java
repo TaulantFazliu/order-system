ab7eebec3b7bcb319842dfb2ede3a35c07a5c945
Taulantfazliu <talifazliu@gmail.com>
2021-12-14 15:43:16 +0100

    changes

--- src/main/java/co/tfProj/orderSystem/user/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package co.tfProj.orderSystem.user.repository;
-
-import co.tfProj.orderSystem.user.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
